e902ff23dbdaf577a0754d43cd5891d938f0a351
DianyelaMaldonado <dianyela.y.maldonado@gmail.com>
2020-10-16 14:38:54 -0500

    I added trial of concept about slider implementation

--- drupal-recipes/src/home/home.js
+++ drupal-recipes/src/home/home.js
@@ -1,4 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
+  slickSlider();
 });
+
+function slickSlider() {
+  console.log("hola")
+  $('div.cardSlider').slick(
+    {
+      centerMode: true,
+      centerPadding: '400px',
+      slidesToShow: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    }
+  );
+}
+
